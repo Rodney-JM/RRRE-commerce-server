ba165789c88f5a7d56171b6b7c3127a10d76c0fe
Rodney <rondineypatricio@gmail.com>
2024-07-29 12:49:47 -0300

    project initial build

---
+++ src/main/java/com/jrm/perfimeEcommerce/PerfimeEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jrm.perfimeEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfimeEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfimeEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrm/perfimeEcommerce/PerfimeEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrm.perfimeEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfimeEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
